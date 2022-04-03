58f620d577ca40e1ee42c8bfec385f8cb20704de
Adamity <50904284+adamity@users.noreply.github.com>
2022-04-03 12:49:04 +0800

    minor change

--- src/App.java
+++ src/App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) {
         TextEditor editor = new TextEditor();
         Thread thread = new Thread(editor);
-        thread.start(); // start the thread
+        thread.start();
     }
 }
